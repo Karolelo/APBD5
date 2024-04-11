43d3f55d12a4a661387a0150de43e04cc18d1b16
Karol Niekrasz <karolniekrasz@gmail.com>
2024-04-11 13:54:26 +0200

    created controllers for visits

--- WebApplication1/WebApplication1/Controllers/VisitController.cs
+++ WebApplication1/WebApplication1/Controllers/VisitController.cs
@@ -1,6 +1,49 @@
-﻿namespace WebApplication1.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Controlers;
 
-public class VisitController
+namespace WebApplication1.Controllers;
+[Route("api/visit")]
+[ApiController]
+public class VisitController: ControllerBase
 {
+    private static readonly List<Visit> _visists = new()
+    {
+        new Visit(1,new DateTime(2002,10,10),1,"Bla bla",20),
+        new Visit(2,new DateTime(2002,10,10),1,"Bla bla",25),
+        new Visit(3,new DateTime(2002,10,10),1,"Bla bla",40),
+        new Visit(4,new DateTime(2002,10,10),2,"Bla bla",30)
+    };
+
+
+
+    [HttpGet("{int:id}")]
+    public IActionResult getVisistOfAnimal(int idOfAnimal)
+    {
+        List<Visit> visitsOfAnimal=new List<Visit>();
+
+        foreach (var visit in _visists)
+        {
+            if (visit.AnimalId == idOfAnimal)
+            {
+                visitsOfAnimal.Add(visit);
+            }
+        }
+
+        if (visitsOfAnimal.Count > 0)
+        {
+            return Ok(visitsOfAnimal);
+        }
+
+        return NotFound($"Not found animal with {idOfAnimal}");
+    }
+
+    [HttpPost]
+    public IActionResult addVisist(Visit visit)
+    {
+        _visists.Add(visit);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+    
+    
     
 }
--- WebApplication1/WebApplication1/Models/Visit.cs
+++ WebApplication1/WebApplication1/Models/Visit.cs
@@ -2,9 +2,24 @@
 
 public class Visit
 {
+    public Visit(int id, DateTime date, int animalId, string description, decimal price)
+    {
+        Id = id;
+        Date = date;
+        AnimalId = animalId;
+        Description = description;
+        Price = price;
+    }
+
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public int AnimalId { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
+    
+    public override string ToString()
+    {
+        return $"Visit: {Id}, Date: {Date.ToShortDateString()}, AnimalId: {AnimalId}, Description: {Description}, Price: {Price:C}";
+    }
+
 }
