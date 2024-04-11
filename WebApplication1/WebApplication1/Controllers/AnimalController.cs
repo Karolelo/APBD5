60983f637bda1e85da560afe583abc0a0785ccac
Karol Niekrasz <karolniekrasz@gmail.com>
2024-04-11 13:36:42 +0200

    created controler for animals

---
+++ WebApplication1/WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+[Route("api/animal")]
+[ApiController]
+public class AnimalController: ControllerBase
+{
+    private static readonly List<Animal> _animals = new()
+    {
+        new Animal(1,"Doggy","Pies",20,"Czarny"),
+        new Animal(2,"Lola","Kot",7,"Biała"),
+        new Animal(3,"Rex","Pies",30,"Czarny")
+    };
+    
+    [HttpGet]
+    public IActionResult GetStudents()
+    {
+        return Ok(_animals);
+    }
+    
+    [HttpGet("{id:int}")]
+    public IActionResult GetAnimal(int id)
+    {
+        var animal = _animals.FirstOrDefault(an => an.id == id);
+
+        if (animal == null)
+        {
+            return NotFound($"Animal with id {id} was not found");
+        }
+
+        return Ok(animal);
+    }
+    
+    [HttpPost]
+    public IActionResult CreateAnimal(Animal animal)
+    {
+        _animals.Add(animal);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+    
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateAnimal(int id, Animal animal)
+    {
+        var animalToEdit= _animals.FirstOrDefault(an=>an.id == id);
+
+        if (animalToEdit == null)
+        {
+            return NotFound($"Animal with id {id} was not found");
+        }
+        
+        _animals.Remove(animalToEdit);
+        _animals.Add(animal);
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteStudent(int id)
+    {
+        var animalToEdit= _animals.FirstOrDefault(an=>an.id == id);
+        if (animalToEdit == null)
+        {
+            return NoContent();
+        }
+
+        _animals.Remove(animalToEdit);
+        return NoContent();
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication1;
+
+public class Animal
+{
+    public Animal(int id, string name, string category, int wage, string color)
+    {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.wage = wage;
+        this.color = color;
+    }
+
+    public int id;
+    public string name;
+    public string category;
+    public int wage;
+    public string color;
+    public override string ToString()
+    {
+        return id + " " + "Imie: " + name + " kategoria: " + category + " waga zwierzaka: " + wage + " kolor " + color;
+    }
+}
